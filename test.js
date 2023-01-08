428eac86ae54e92ec46e7a9f085c595ba72f4580
ridoy113 <almamounhridoy113@gmail.com>
2023-01-08 12:30:10 +0600

    try js set srtacture

--- test.js
+++ test.js
@@ -6,51 +6,51 @@ console.log("hello"); */
 
 // let sentence = `I love ${a} it is a big country`;
 
-function getRenNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-// console.log(getRenNumber(1,6));
+// function getRenNumber(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+// // console.log(getRenNumber(1,6));
 
-const student = ["Ridoy", "Kamal", "Faroque", "Nayem", "Asrafull"];
-// console.log(student.sort());
+// const student = ["Ridoy", "Kamal", "Faroque", "Nayem", "Asrafull"];
+// // console.log(student.sort());
 
-const roll_numbers = [223, 344, 453, 56, 3434, 565, 57, 345];
-// console.log(roll_numbers.sort(function (a, b) {
-//     return a - b;
-// }));
+// const roll_numbers = [223, 344, 453, 56, 3434, 565, 57, 345];
+// // console.log(roll_numbers.sort(function (a, b) {
+// //     return a - b;
+// // }));
 
-function leapYear(year) {
-  return year % 100 === 0 ? year % 400 === 0 : year % 4 === 0;
-} // console.log(leapYear(2016));
+// function leapYear(year) {
+//   return year % 100 === 0 ? year % 400 === 0 : year % 4 === 0;
+// } // console.log(leapYear(2016));
 
-const vowel = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+// const vowel = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
 
-function countVowel(sentence) {
-  let count = 0;
-  const letters = Array.from(sentence);
+// function countVowel(sentence) {
+//   let count = 0;
+//   const letters = Array.from(sentence);
 
-  letters.forEach(function (value, index, array) {
-    if (vowel.includes(value)) {
-      count++;
-    }
-  });
-  return count;
-}
-// console.log(countVowel("I love Bangladesh"));
+//   letters.forEach(function (value, index, array) {
+//     if (vowel.includes(value)) {
+//       count++;
+//     }
+//   });
+//   return count;
+// }
+// // console.log(countVowel("I love Bangladesh"));
 
-const numbers = [1, 54, 5, 4, 5, 2, 56, 44, 44, 56, 99, 99];
+// const numbers = [1, 54, 5, 4, 5, 2, 56, 44, 44, 56, 99, 99];
 
-const duplicates = numbers.filter(function (value, index, array) {
-  return array.indexOf(value) !== index;
-});
+// const duplicates = numbers.filter(function (value, index, array) {
+//   return array.indexOf(value) !== index;
+// });
 
-// console.log(duplicates);
+// // console.log(duplicates);
 
-const number = [1, 54, 5, 4, 5, 2, 56, 44, 44, 56, 99, 99];
+// const number = [1, 54, 5, 4, 5, 2, 56, 44, 44, 56, 99, 99];
 
-const unique = number.filter(function (value, index, array) {
-  return array.indexOf(value) === index;
-});
+// const unique = number.filter(function (value, index, array) {
+//   return array.indexOf(value) === index;
+// });
 
 // console.log(unique);
 
@@ -129,3 +129,23 @@ const unique = number.filter(function (value, index, array) {
 //   text += cars[i] + "<br>";
 // }
 // console.log(text);
+
+// const mySet = new Set();
+
+// mySet.add("a");
+// mySet.add("b");
+// mySet.add("c");
+// mySet.add("d");
+
+// console.log(mySet);
+
+// Create a Set
+const letters = new Set(["a", "b", "c"]);
+
+// // List all Elements
+// let text = "";
+// letters.forEach(function (value) {
+//   text += value;
+// });
+
+console.log(letters.values());
