5169ef9c49a5df4962fd2fb5ca16e97caf9b4a39
ridoy113 <almamounhridoy113@gmail.com>
2023-01-10 13:43:55 +0600

    use js regExp

--- test.js
+++ test.js
@@ -196,3 +196,22 @@ console.log(y);
 let a = 10e6;
 console.log(typeof a.toExponential());
 */
+// console.log(!false);
+
+// -----RegExp-----
+/*
+let text = "Visit W3Schools!";
+let n = text.search("W3Schools");
+console.log(n);
+let text = "Visit W3Schools";
+let n = text.search(/w3schools/i);
+console.log(n);
+let text = "Visit Microsoft!";
+let result = text.replace("Microsoft", "W3Schools");
+console.log(result);
+let text = "Visit Microsoft!";
+let result = text.replace(/microsoft/i, "W3Schools");
+console.log(result);
+*/
+
+
